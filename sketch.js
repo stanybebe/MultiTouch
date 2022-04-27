566deab1d033c94cda370ebe3663d78471262764
stanybebe <tristan.e.whitehill@gmail.com>
2022-04-26 23:34:31 -0400

    up and runnin

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
+var x1 =0;
+var y1 = 0;
+var x2 = 0;
+var y2 = 0;
+var noneFlag = 0;
 function setup() {
   // set canvas to window size
-  createCanvas(windowWidth, windowHeight);
+
+var width = myCanvas.offsetWidth;
+ var sketchCanvas = createCanvas(width,700, WEBGL);
+ sketchCanvas.parent("myCanvas");
 }
 
 function draw() {
@@ -8,17 +16,28 @@ function draw() {
 
   fill('blue');
   if (touches.length<3){
+   
   for (var i = 0; i < touches.length; i++) {
     noStroke();
-    ellipse(touches[i].x, touches[i].y, 50, 50);
+    ellipse(touches[i].x-width/2, touches[i].y-height/2, 50, 50);
   }
   }
   else{
     for (var i = 0; i < 2; i++) {
     noStroke();
-    ellipse(touches[i].x, touches[i].y, 50, 50);
+    ellipse(touches[i].x-width/2, touches[i].y-height/2, 50, 50);
   }
   }
+
+  if (touches.length>0){
+      x1 = touches[0].x;
+      noneFlag = 0;
+  }
+  else{
+    noneFlag = 1;
+  }
+  textSize(50);
+  text(x1, 10, 10, 70, 80);
 }
 
 // do this prevent default touch interaction
