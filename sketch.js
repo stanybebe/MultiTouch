3bcfaadbb12bc3c5ba890046dac2a4de7dcacc40
stanybebe <tristan.e.whitehill@gmail.com>
2022-05-14 15:00:50 -0400

    ui and distance

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ var x2 = 0;
 var y2 = 0;
 var noneFlag = 0;
 var noneFlag2 = 0;
+var sizeA = 0;
+var sizeB = 0;
+
+
 function setup() {
   // set canvas to window size
 
@@ -14,51 +18,115 @@ var width = myCanvas.offsetWidth;
 var mapRange = function(from, to, s) {
   return to[0] + (s - from[0]) * (to[1] - to[0]) / (from[1] - from[0]);
 };
+// d = √[(x2 − x1)2 + (y2 − y1)2]
+var distance = function(x1,y1,x2,y2){
+  return sqrt((x2-x1)*2+((y2-y1)*2));
+}
 function draw() {
   
+background(255);
   
-  // background('beige');
-  fill(245,245,220,50)
-  rect(0,0,width,height);
 
-  
   if (touches.length<3){
    
   for (var i = 0; i < touches.length; i++) {
-    fill('blue');
-    noStroke();
-    ellipse(touches[i].x, touches[i].y, 50, 50);
+    noFill();
+    stroke(0,0,0, sizeA);
+    strokeWeight(1);
+    ellipse(touches[i].x, touches[i].y, sizeA, sizeA);
   }
   }
+  //if touches are more than 2 still draw the circles
   else{
     for (var i = 0; i < 2; i++) {
-      fill('blue');
-      noStroke();
-    ellipse(touches[i].x, touches[i].y, 50, 50);
+      noFill();
+      stroke(0,0,0, sizeA);
+      strokeWeight(1);
+      ellipse(touches[i].x, touches[i].y, sizeB, sizeB);
+    
   }
   }
 
   if (touches.length>0){
+
       x1 = touches[0].x;
       y1 = touches[0].y;
       noneFlag = 0;
+    
   }
   else{
+
     noneFlag = 1;
+    noneFlag2 = 1;
   }
 
   if (touches.length>1){
+
       x2 = touches[1].x;
       y2 = touches[1].y;
       noneFlag2 = 0;
+      var d = distance(touches[0].x,touches[0].y,touches[1].x,touches[1].y);
   }
 
-  if (touches.length!=2){
+
+  if (touches.length>2){
+    x2 = touches[touches.length-1].x;
+    y2 = touches[touches.length-1].y;
+    noneFlag2 = 0;
+}
+
+  if (touches.length==0){
+    // noneFlag = 1;
      noneFlag2 = 1;
+
+    
+}
+if (noneFlag==0){
+  sizeA=sizeA+2;
+     
+  if (sizeA>=200){
+    sizeA=200;
+  }
+
+}
+else{
+    sizeA=sizeA-10;
+    if (sizeA<=0){
+      sizeA=0;
+    }
+    stroke(0,0,0, sizeA);
+    strokeWeight(1);
+    ellipse(x1, y1, sizeA, sizeA);
+}
+if(noneFlag==0&&noneFlag2==0){
+  stroke(0,0,0, sizeB);
+  line(x1, y1, x2, y2);
+  }
+if (noneFlag2==0){
+  
+  
+ 
+  sizeB=sizeB+2;
+     
+  if (sizeB>=200){
+    sizeB=200;
+  }
+
+}
+else{
+    sizeB=sizeB-10;
+    if (sizeB<=0){
+      sizeB=0;
+    }
+    stroke(0,0,0, sizeA);
+    strokeWeight(1);
+    ellipse(x2, y2, sizeB, sizeB);
 }
 
 
-  
+
+
+  var mapD = mapRange([0,50],[1,200],d);
   var x1m = mapRange ([0,width],[0,9000],x1);
   var x2m = mapRange ([0,width],[0,9000],x2);
 
@@ -66,6 +134,7 @@ function draw() {
   Pd.send('freq2',[parseFloat(y2)]);
   Pd.send('filter',[parseFloat(x1m)]);
   Pd.send('filter2',[parseFloat(x2m)]);
+  Pd.send('d',[parseFloat(mapD)]);
 
   //if no notes are on 
   if(noneFlag == true){
@@ -81,8 +150,11 @@ function draw() {
    else{
      Pd.send('on2',[parseFloat(1)]);
    }
+
+  push();
   textSize(32);
-  text(x1m,50,50);
+  text(mapD,50,50);
+  pop();
   
   
 }
